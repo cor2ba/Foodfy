22dd340bb1222d90e89bba1a06637a68d3b4ff79
cor2ba <cordobagabrielignacio@gmail.com>
2022-07-31 23:08:38 -0300

    cambios deploy

--- client/src/index.js
+++ client/src/index.js
@@ -4,6 +4,11 @@ import { BrowserRouter as Router } from "react-router-dom";
 import { Provider } from "react-redux";
 import App from "./App";
 import store from "./redux/store";
+import dotenv from "dotenv";
+dotenv.config();
+
+axios.defaults.baseURL = process.env.REACT_APP_API || "http://localhost:3001";
+
 
 ReactDOM.render(
   <Provider store={store}>
