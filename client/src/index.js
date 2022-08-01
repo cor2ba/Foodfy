06b4d884c2c20b49fd697165943f6e3d6f9d151b
cor2ba <cordobagabrielignacio@gmail.com>
2022-08-01 00:01:20 -0300

    cambios deploy

--- client/src/index.js
+++ client/src/index.js
@@ -5,6 +5,7 @@ import { Provider } from "react-redux";
 import App from "./App";
 import store from "./redux/store";
 import dotenv from "dotenv";
+import axios from "axios"
 dotenv.config();
 
 axios.defaults.baseURL = process.env.REACT_APP_API;
